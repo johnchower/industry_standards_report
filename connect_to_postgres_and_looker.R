5a83190df84a5554503894481ebfdd85d25e54ec
johnchower <johnchower@gmail.com>
2016-09-26 15:02:21 -0600

    Initial commit

---
+++ connect_to_postgres_and_looker.R
@@ -0,0 +1,19 @@
+library(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")
+
+pass <- readline(prompt = "Enter password: ")
+
+con <- 
+  dbConnect(
+    drv
+    , dbname="polymer_production"
+    , host="localhost"
+    , port=5442
+    , user="jhower"
+    , password=pass
+)
+
+looker_setup(id = "mz8bPyHzY7MY5JNhSmcj",
+             secret = "vttZrNKZGcCyB8xyGq68qfVR",
+             api_path = "https://api-looker.gloo.us/api/3.0")
